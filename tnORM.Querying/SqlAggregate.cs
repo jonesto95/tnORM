ef4e09d956697a9f52d6b9bb82a6ee07df8f59ab
Joseph Onesto <jonesto95@gmail.com>
2022-05-11 21:34:13 -0500

    Added native Sql Function and Sql Aggregate support

---
+++ tnORM.Querying/SqlAggregate.cs
@@ -0,0 +1,138 @@
+﻿namespace tnORM.Querying
+{
+    public static class SqlAggregate
+    {
+        #region COUNT Functions
+
+        public static RawSqlText Count()
+        {
+            return Count("*");
+        }
+
+
+        public static RawSqlText CountDistinct(string fieldName)
+        {
+            return Count($"DISTINCT {fieldName}");
+        }
+
+
+        public static RawSqlText Count(string fieldName)
+        {
+            string sql = $"COUNT({fieldName})";
+            return new RawSqlText(sql);
+        }
+
+        #endregion
+
+
+        #region SUM Functions
+
+        public static RawSqlText Sum()
+        {
+            return Sum("*");
+        }
+
+
+        public static RawSqlText SumDistinct(string fieldName)
+        {
+            return Sum($"DISTINCT {fieldName}");
+        }
+
+
+        public static RawSqlText Sum(string fieldName)
+        {
+            string sql = $"SUM({fieldName})";
+            return new RawSqlText(sql);
+        }
+
+        #endregion
+
+
+        #region MIN Functions
+
+        public static RawSqlText Min()
+        {
+            return Min("*");
+        }
+
+
+        public static RawSqlText MinDistinct(string fieldName)
+        {
+            return Min($"DISTINCT {fieldName}");
+        }
+
+
+        public static RawSqlText Min(string fieldName)
+        {
+            string sql = $"MIN({fieldName})";
+            return new RawSqlText(sql);
+        }
+
+        #endregion
+
+
+        #region MAX Functions
+
+        public static RawSqlText Max()
+        {
+            return Max("*");
+        }
+
+
+        public static RawSqlText MaxDistinct(string fieldName)
+        {
+            return Max($"DISTINCT {fieldName}");
+        }
+
+
+        public static RawSqlText Max(string fieldName)
+        {
+            string sql = $"MAX({fieldName})";
+            return new RawSqlText(sql);
+        }
+
+        #endregion
+
+
+        #region AVG Functions
+
+        public static RawSqlText Avg()
+        {
+            return Avg("*");
+        }
+
+
+        public static RawSqlText AvgDistinct(string fieldName)
+        {
+            return Avg($"DISTINCT {fieldName}");
+        }
+
+
+        public static RawSqlText Avg(string fieldName)
+        {
+            string sql = $"AVG({fieldName})";
+            return new RawSqlText(sql);
+        }
+
+
+        public static RawSqlText Average()
+        {
+            return Average("*");
+        }
+
+
+        public static RawSqlText AverageDistinct(string fieldName)
+        {
+            return Average($"DISTINCT {fieldName}");
+        }
+
+
+        public static RawSqlText Average(string fieldName)
+        {
+            string sql = $"AVG({fieldName})";
+            return new RawSqlText(sql);
+        }
+
+        #endregion
+    }
+}
---
+++ tnORM.Querying/SqlFunction.cs
@@ -0,0 +1,87 @@
+﻿using tnORM.Shared;
+
+namespace tnORM.Querying
+{
+    public static class SqlFunction
+    {
+        public static RawSqlText Round(string fieldName, int decimalPlaces)
+        {
+            string sql = $"ROUND({fieldName}, {decimalPlaces})";
+            return new RawSqlText(sql);
+        }
+
+
+        public static RawSqlText Uppercase(string fieldName)
+        {
+            return Upper(fieldName);
+        }
+
+
+        public static RawSqlText Upper(string fieldName)
+        {
+            string sql = $"UPPER({fieldName})";
+            return new RawSqlText(sql);
+        }
+
+
+        public static RawSqlText Lowercase(string fieldName)
+        {
+            return Lower(fieldName);
+        }
+
+
+        public static RawSqlText Lower(string fieldName)
+        {
+            string sql = $"LOWER({fieldName})";
+            return new RawSqlText(sql);
+        }
+
+
+        public static RawSqlText Substring(string fieldName, int startIndex, int length)
+        {
+            string sql = $"SUBSTRING({fieldName}, {startIndex}, {length})";
+            return new RawSqlText(sql);
+        }
+
+
+        public static RawSqlText Length(string fieldName)
+        {
+            return Len(fieldName);
+        }
+
+
+        public static RawSqlText Len(string fieldName)
+        {
+            string sql = $"LEN({fieldName})";
+            return new RawSqlText(sql);
+        }
+
+
+        public static RawSqlText GetDate()
+        {
+            string sql = $"GETDATE()";
+            return new RawSqlText(sql);
+        }
+
+
+        public static RawSqlText GetUtcDate()
+        {
+            string sql = $"GETUTCDATE()";
+            return new RawSqlText(sql);
+        }
+
+
+        public static RawSqlText Format(string fieldName, string format)
+        {
+            string sql = $"FORMAT({fieldName}, {format.ToSqlString()})";
+            return new RawSqlText(sql);
+        }
+
+
+        public static RawSqlText Format(string fieldName, string format, string culture)
+        {
+            string sql = $"FORMAT({fieldName}, {format.ToSqlString()}, {culture.ToSqlString()})";
+            return new RawSqlText(sql);
+        }
+    }
+}
